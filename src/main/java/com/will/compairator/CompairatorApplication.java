180feb6657005e60488f63a0f854cc5cc0e1fbfc
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-06-08 15:47:23 +0200

    Initial commit

---
+++ src/main/java/com/will/compairator/CompairatorApplication.java
@@ -0,0 +1,13 @@
+package com.will.compairator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompairatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompairatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/compairator/CompairatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.compairator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompairatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
